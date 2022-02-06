cf554590f0e431ee9cefffe84cba20de52300bde
VivekBits2210 <viveknayak2210@gmail.com>
2022-02-06 15:32:20 +0530

    Adding basic game plan

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # poker-bot
-Bot for playing professional poker
+Bot for playing professional poker, impervious to bot detectors.
---
+++ __main__.py
@@ -0,0 +1,25 @@
+# TODO: Watch Yannic video https://www.youtube.com/watch?v=BhUWvQmLzSk
+# TODO: Implement CFRM for rock paper scissors
+# TODO: Implement CFRM for tic tac toe https://colab.research.google.com/drive/1SYNxGdR7UmoxbxY-NSpVsKywLX7YwQMN?usp=sharing
+# TODO: Get frequency of betting and differences in strategies etc in noob poker games
+# TODO: Rewrite code for simple pluribus
+# TODO: Look at complex pluribus
+#  https://github.com/keithlee96/pluribus-poker-AI
+#  https://github.com/Karsens/pluribus-poker-bot-js
+#  https://github.com/fedden/poker_ai
+#  https://github.com/zanussbaum/pluribus
+#  https://github.com/Agnar22/Pluribus
+# TODO: Look at generic poker bots
+#  https://github.com/IanSullivan/PokerCFR
+#  https://github.com/ozzi7/Poker-MCCFRM
+#  https://github.com/OpenHoldem/openholdembot
+# TODO: Look closely at subgame solving https://arxiv.org/abs/1705.02955 https://www.youtube.com/watch?v=6ozyX5M35og
+# TODO: Build poker bot based on code
+# TODO: Try bot-to-bot poker against existing repos https://github.com/dickreuter/Poker
+# TODO: In the poker bot, switch between exploitation tactics and nash equilibrium (2 separate bots)
+# TODO: Deep CFRM https://arxiv.org/pdf/1811.00164.pdf
+# TODO: Deep poker bots in general
+#  https://github.com/dickreuter/neuron_poker
+#  https://github.com/MoniFarsang/poker-bot
+#  https://github.com/garyjh126/MLFYP_Project
+#  https://github.com/datamllab/rlcard
